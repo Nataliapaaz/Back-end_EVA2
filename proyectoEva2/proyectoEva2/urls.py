e6860333a539b18e08abe7b540f495d04b40e35f
Natalia Paredes <natalia.paz.paredes@hotmail.com>
2023-11-07 19:55:51 -0300

    Creo que conecte bien la base de datos

--- proyectoEva2/proyectoEva2/urls.py
+++ proyectoEva2/proyectoEva2/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from formularioRamos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('formularioramos/', views.renderTemplate)
 ]
